83dfad804f14c3052d3443b5b0b5502582fabaac
minhtriet <minhtriet09@gmail.com>
2022-12-10 23:20:21 +0000

    ch: add constants file

---
+++ src/constants.py
@@ -0,0 +1,3 @@
+MAX_LINES_ALLOWED_CENSUS = 2
+CENSUS_BATCH_URL = "https://geocoding.geo.census.gov/geocoder/geographies/addressbatch"
+CENSUS_GEOCODER_FROM_COORD = "https://geocoding.geo.census.gov/geocoder/geographies/coordinates"
