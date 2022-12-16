bb65f04b4bab68e756c391cb77872826594c84eb
minhtriet <minhtriet09@gmail.com>
2022-12-16 23:52:20 +0000

    c: add tmp file names

--- src/constants.py
+++ src/constants.py
@@ -1,6 +1,9 @@
 import logging
 
-MAX_LINES_ALLOWED_CENSUS = 40  # 10000
+MAX_LINES_ALLOWED_CENSUS = 10000
 LOGGING_LEVEL = logging.DEBUG  # DEBUG/INFO for more/less detailed log
 CENSUS_BATCH_URL = "https://geocoding.geo.census.gov/geocoder/geographies/addressbatch"
 CENSUS_GEOCODER_FROM_COORD = "https://geocoding.geo.census.gov/geocoder/geographies/coordinates"
+
+TEMP_UNMATCH_CSV = "unmatched_census.csv"
+TEMP_MATCH_CSV = "matched_census.csv"
