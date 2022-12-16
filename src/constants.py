7ca86563767cd70d94c2eb5cb792e80c5efcee34
minhtriet <minhtriet09@gmail.com>
2022-12-16 22:03:58 +0000

    c: add more constants

--- src/constants.py
+++ src/constants.py
@@ -1,3 +1,6 @@
-MAX_LINES_ALLOWED_CENSUS = 2
+import logging
+
+MAX_LINES_ALLOWED_CENSUS = 40  # 10000
+LOGGING_LEVEL = logging.DEBUG  # DEBUG/INFO for more/less detailed log
 CENSUS_BATCH_URL = "https://geocoding.geo.census.gov/geocoder/geographies/addressbatch"
 CENSUS_GEOCODER_FROM_COORD = "https://geocoding.geo.census.gov/geocoder/geographies/coordinates"
