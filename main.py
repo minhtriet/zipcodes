039e3a10b977f409eb3de270dd837b5f0e03a27a
minhtriet <minhtriet09@gmail.com>
2022-12-16 23:47:24 +0000

    ft: validate filename and output intermediate dfs

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import argparse
 import asyncio
 import logging
+import os.path
 from pathlib import Path
 
 import pandas as pd
@@ -10,6 +11,7 @@ import src.constants
 from src.data_handler import DataHandler
 from src.geocoder import Geocoder
 
+
 async def process_umatched_csv(df):
     gs = Geocoder()
     if "Street address" not in df.columns:
@@ -18,15 +20,24 @@ async def process_umatched_csv(df):
     return tuples
 
 
+def validate_filename(filename):
+    if not os.path.exists(filename):
+        raise ValueError(f"Cannot find file {filename} in path {os.getcwd()}")
+    if (os.path.exists(src.constants.TEMP_UNMATCH_CSV)) and (filename != src.constants.TEMP_UNMATCH_CSV):
+        raise ValueError(f'''Found a file named {src.constants.TEMP_UNMATCH_CSV}, which is a result of the 
+            previous\n run. You may want to pass it in instead. To ignore this error, please delete \n
+            {src.constants.TEMP_UNMATCH_CSV} first and run the code again.''')
+    return filename
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Address to Tracts, Block Groups and Blocks')
-    parser.add_argument('-f', type=str, default='portion_addr.csv',
+    parser.add_argument('-f', default='processed_address.csv', type=validate_filename,
                         help='''
                         The path of the addresses file, which should not have header columns. However, according to
                         https://geocoding.geo.census.gov/geocoder/Geocoding_Services_API.html#_Toc7768597,
                         the columns are `Unique ID` (Just as a reference), `Street address`, `City`, `State`, `ZIP`
                         ''')
-
     args = parser.parse_args()
 
     logging.getLogger(__name__)
@@ -37,9 +48,15 @@ if __name__ == "__main__":
     result_with_columns = asyncio.run(dh.batch_process_csv(args.f))
 
     matched = result_with_columns[result_with_columns['is_matched'] == 'Match'].drop('is_matched', axis=1)
+    # Construct column "blockgroup" from 1st char of column "block"
     matched['blockgroup'] = matched['block'].astype(str).str[0]
+    matched.to_csv(src.constants.TEMP_MATCH_CSV, index=False)
 
     unmatched_and_tie = result_with_columns[result_with_columns['is_matched'] != 'Match'].drop('is_matched', axis=1)
+    unmatched_and_tie.to_csv(src.constants.TEMP_UNMATCH_CSV, index=False)
+
+    logging.info(f'''There are still {len(unmatched_and_tie)} addresses that Census could not find a match.\n
+    They need to be posted to Google service.''')
     tracts = asyncio.run(process_umatched_csv(unmatched_and_tie))
     unmatched_and_tie[['tract', 'blockgroup', 'block', 'autocorrected_addr', 'same_addr']] = tracts
 
