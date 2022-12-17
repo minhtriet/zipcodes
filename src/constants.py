ed1ce5ad32ae32dd798131490cb9745034691634
minhtriet <minhtriet09@gmail.com>
2022-12-17 01:05:06 +0000

    test with lower number of lines

--- src/constants.py
+++ src/constants.py
@@ -1,6 +1,6 @@
 import logging
 
-MAX_LINES_ALLOWED_CENSUS = 10000
+MAX_LINES_ALLOWED_CENSUS = 1500  # 10000
 LOGGING_LEVEL = logging.DEBUG  # DEBUG/INFO for more/less detailed log
 CENSUS_BATCH_URL = "https://geocoding.geo.census.gov/geocoder/geographies/addressbatch"
 CENSUS_GEOCODER_FROM_COORD = "https://geocoding.geo.census.gov/geocoder/geographies/coordinates"
