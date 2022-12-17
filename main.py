af8a5336c7ccc70f0284d52e59a966513a1b72cb
minhtriet <minhtriet09@gmail.com>
2022-12-17 01:09:09 +0000

    f: add capability to resume running

--- main.py
+++ main.py
@@ -24,11 +24,12 @@ async def process_unmatched_csv(df):
 
 def validate_filename(filename):
     if not os.path.exists(filename):
-        raise ValueError(f"Cannot find file {filename} in path {os.getcwd()}")
-    if (os.path.exists(src.constants.TEMP_UNMATCH_CSV)) and (filename != src.constants.TEMP_UNMATCH_CSV):
-        raise ValueError(f'''Found a file named {src.constants.TEMP_UNMATCH_CSV}, which is a result of the 
-            previous\n run. You may want to pass it in instead. To ignore this error, please delete \n
-            {src.constants.TEMP_UNMATCH_CSV} first and run the code again.''')
+        raise argparse.ArgumentTypeError(f"Cannot find file {filename} in path {os.getcwd()}")
+    if (os.path.exists(src.constants.TEMP_UNMATCH_CSV)) and (os.path.exists(src.constants.TEMP_MATCH_CSV)):
+        if (filename != src.constants.TEMP_UNMATCH_CSV):
+            raise argparse.ArgumentTypeError(f'''Found a file named {src.constants.TEMP_UNMATCH_CSV}, which is a result
+                of the previous run. You may want to pass it in instead. To overcome this error, please delete both
+                {src.constants.TEMP_MATCH_CSV} and {src.constants.TEMP_UNMATCH_CSV} first and run the code again.''')
     return filename
 
 
@@ -46,20 +47,24 @@ if __name__ == "__main__":
     logging.root.setLevel(src.constants.LOGGING_LEVEL)
 
     dh = DataHandler()
-    # split the CSV and pushed the chunks to Census
-    result_with_columns = asyncio.run(dh.batch_process_csv(args.f))
+    if args.f != src.constants.TEMP_UNMATCH_CSV:
+        # split the CSV and pushed the chunks to Census
+        result_with_columns = asyncio.run(dh.batch_process_csv(args.f))
 
-    matched = result_with_columns[result_with_columns['is_matched'] == 'Match'].drop('is_matched', axis=1)
-    # Construct column "blockgroup" from 1st char of column "block"
-    matched['blockgroup'] = matched['block'].astype(str).str[0]
-    matched.to_csv(src.constants.TEMP_MATCH_CSV, index=False)
+        matched = result_with_columns[result_with_columns['is_matched'] == 'Match'].drop('is_matched', axis=1)
+        # Construct column "blockgroup" from 1st char of column "block"
+        matched['blockgroup'] = matched['block'].astype(str).str[0]
+        matched.to_csv(src.constants.TEMP_MATCH_CSV, index=False)
 
-    unmatched_and_tie = result_with_columns[result_with_columns['is_matched'] != 'Match'].drop('is_matched', axis=1)
-    unmatched_and_tie.to_csv(src.constants.TEMP_UNMATCH_CSV, index=False)
+        unmatched_and_tie = result_with_columns[result_with_columns['is_matched'] != 'Match'].drop('is_matched', axis=1)
+        unmatched_and_tie.to_csv(src.constants.TEMP_UNMATCH_CSV, index=False)
+    else:
+        matched = pd.read_csv(src.constants.TEMP_MATCH_CSV)
+        unmatched_and_tie = pd.read_csv(src.constants.TEMP_UNMATCH_CSV)
 
     logging.info(f'''There are still {len(unmatched_and_tie)} addresses that Census could not find a match.\n
     They need to be posted to Google service.''')
-    tracts = asyncio.run(process_umatched_csv(unmatched_and_tie))
+    tracts = asyncio.run(process_unmatched_csv(unmatched_and_tie))
     unmatched_and_tie[['tract', 'blockgroup', 'block', 'autocorrected_addr', 'same_addr']] = tracts
 
     final_result = pd.concat([matched, unmatched_and_tie])
